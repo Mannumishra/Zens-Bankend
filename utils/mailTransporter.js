fac9dbba56a4bbd2d65b62421f24daa6e4f41fec
mannumishra <mannumishra2207@outlook.com>
2024-09-07 15:54:31 +0530

    add chnages

--- utils/mailTransporter.js
+++ utils/mailTransporter.js
@@ -2,7 +2,7 @@ const nodemailer = require("nodemailer")
 
 const transporter = nodemailer.createTransport({
     host: process.env.HOST,
-    port: process.env.PORT,
+    port: process.env.SMPT_PORT,
     auth: {
         user: process.env.MAIL_SEND,
         pass: process.env.MAIL_PASS
